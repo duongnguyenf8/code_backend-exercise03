1afcd4d6ac283c0934221fca387516c3b70b1a1a
duongnguyenf8 <nguyenduong@fullstack.edu.vn>
2023-08-19 21:24:51 +0700

    Chữa bài tập về nhà backend-exercise03

---
+++ model/index.js
@@ -0,0 +1,30 @@
+const htmlController = require("../controllers/htmlControllers.js");
+const assetsController = require("../controllers/assetsControllers");
+const todoController = require("../controllers/todoControllers");
+/**
+ * Defines the routes for the home page
+ */
+const index = {
+  assets: (req, res, pathname, pathView) => {
+    if (req.method === "GET") {
+      if (!pathname.startsWith("/assets/")) {
+        pathname = pathname.slice(pathname.lastIndexOf("/assets/"));
+      }
+      assetsController(res, pathname, pathView);
+    } else {
+      res.writeHead(405, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Phương thức không được hỗ trợ");
+    }
+  },
+  html: (req, res, pathView, pathFile, pathname) => {
+    if (req.method === "GET") {
+      htmlController(req, res, pathView, pathFile, pathname);
+    } else {
+      res.writeHead(405, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Phương thức không được hỗ trợ");
+    }
+  },
+  todo: todoController,
+};
+
+module.exports = index;
